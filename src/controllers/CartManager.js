f2e6cf3359ed7ea7794a62a183250bb19b93716f
luisz84 <lzaramella.1984@gmail.com>
2023-02-20 18:41:37 -0300

    V1

---
+++ src/controllers/CartManager.js
@@ -0,0 +1,74 @@
+import {promises as fs} from 'fs';
+import  {nanoid}  from 'nanoid';
+import ProductManager from './ProductManager.js';
+
+const productAll= new ProductManager
+
+class CartManager 
+{
+    
+     constructor(){
+        this.path = "src/controllers/models/carts.json"
+     } 
+     
+     readCarts=async()=>{
+        let carts=  await fs.readFile(this.path, "utf-8")
+        return JSON.parse(carts)     
+    }
+    
+    writeCarts= async(carts)=>{
+        await fs.writeFile(this.path, JSON.stringify(carts))
+    }
+    exist= async(id)=>{
+        let carts= await this.readCarts();
+        return  carts.find(cart => cart.id=== id)
+    }
+
+    addCarts = async (carts)=>{
+        let cartsOld = await this.readCarts(carts); 
+        let id = nanoid()
+        let cartsConcat= [{id:id, product:[]},...cartsOld]
+        await this.writeCarts(cartsConcat)
+        return "Carrito Agregado"
+    }
+    getCartsById= async(id) =>{
+        let cartById= await this.exist(id)
+        if (!cartById)  return "Carrito no encontrado"
+        return cartById
+        console.log(cartById)
+       
+    }
+    addProductsInCart= async(cartId, productId) => {
+        let cartById= await this.exist(cartId)
+        if (!cartById)  return "Carrito no encontrado"
+        let productById= await productAll.exist(productId)
+        if (!productById)  return "Producto no encontrado"
+        let cartsAll= await this.readCarts()
+        let cartFilter= cartsAll.filter(prod=> prod.id !=productId)
+        let cartsConcat= [{id:cartId,products:[{id:productById.id, cantidad: 1 }]},...cartFilter]
+        await this.writeCarts(cartsConcat)
+        return "producto agregado al carrito"
+
+
+
+        if(cartById.products.some(prod => prod.id === productById)){
+            let productInCart = cartById.products.find(prod=> prod.id === productId)
+            productInCart.cantidad++
+            let cartsConcat= [productInCart,...carFilter]
+            await this.writeCarts(cartsConcat)
+            return "producto sumado al carrito"
+        // }
+
+
+
+        // let carFilter= cartsAll.filter(prod => prod.id != productId)
+
+        //  let carConcat= [{id:cartId,products:[{id:productById.id, cantidad: 1 }]},...carFilter]   
+
+        //  await this.writeCarts(cartsConcat)
+        //  return "producto agregado al carrito"
+    }
+
+}
+}
+export default CartManager
---
+++ src/controllers/ProductManager.js
@@ -0,0 +1,64 @@
+import {promises as fs} from 'fs';
+import  {nanoid}  from 'nanoid';
+
+ class ProductManager{
+    constructor(){
+        this.path= "./src/controllers/models/products.json"
+    }
+
+    readProducts=async()=>{
+        let products=  await fs.readFile(this.path, "utf-8")
+        return JSON.parse(products)     
+    }
+    
+    writeProducts= async(product)=>{
+        await fs.writeFile(this.path, JSON.stringify(product))
+    }
+    exist= async(id)=>{
+        let products= await this.readProducts();
+        return  products.find(product=> product.id=== id)
+    }
+    
+    addProducts= async(product) =>{
+        let productsOld= await this.readProducts()
+        product.id= nanoid(10)  
+        let productAll = [...productsOld, product]
+        await this.writeProducts(productAll)
+        return "producto Agregado"
+        
+        return "producto agregado";
+    }
+    getProducts= async() =>{
+        return await this.readProducts()
+    }
+
+    getProductsById= async(id) =>{
+         
+        let productById= await this.exist(id)
+        if (!productById) return "producto no encontrado"
+        return productById
+    }
+
+    updateProducts= async(id,product)=>{
+        let productById= await this.exist(id)
+        await this.deleteProducts(id)
+        let productOld= await this.readProducts()
+        let products= [{...product,id:id}, ...productOld]
+        await this.readProducts(products)
+    }
+    deleteProducts= async(id) =>{
+        let products= await this.readProducts()
+        let existeProducts= products.some(prod => prod.id=== id)
+        if(existeProducts){
+        let filterProducts= products.filter(prod=> prod.id != id)
+        await this.writeProducts(filterProducts)
+        return "producto eliminado"
+        }
+        return "Producto a eliminar no existe"
+    }
+}
+
+export default ProductManager
+
+
+
